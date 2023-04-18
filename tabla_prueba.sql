ab207cfc41d0f0bfed49c091f343b120563886d4
Pfrigate <daniel.rivero132@gmail.com>
2023-04-18 13:49:18 -0600

    tabla corregida

--- tabla_prueba.sql
+++ tabla_prueba.sql
@@ -6,6 +6,17 @@ inventario varchar(255),
 posicion_fisica varchar(255),
 usuario varchar(255)
 )
+create table Bitacora(
+    ID int NOT NULL auto_increment,
+    piso varchar(255),
+    area varchar(255),
+    nombre_usuario varchar(255),
+    tipo_usr varchar(255),
+    actividad varchar(255),
+    medio varchar(255),
+    comentario varchar(255),
+    PRIMARY KEY (ID)
+);
 
 LOAD DATA INFILE 'C:/Users/admin/Desktop/Bot_Telegram/prueba1.csv'
 INTO TABLE prueba1
